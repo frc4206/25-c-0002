44b37d6a6d557ebbbeec9f226cdcfc5485dd532e
cmoliverio <cmoliverio.10@gmail.com>
2025-01-08 21:00:47 -0600

    Initial commit

---
+++ src/main/java/frc/robot/BuildVersion.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildVersion {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "25-c-0000";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 1;
+    public static final String GIT_SHA = "5258f2d8c180d21ec09564aeb7d65d4d96e8b3fb";
+    public static final String GIT_DATE = "2025-01-08 20:06:17 CST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2025-01-08 21:00:01 CST";
+    public static final long BUILD_UNIX_TIME = 1736391601662L;
+    public static final int DIRTY = 1;
+
+    private BuildVersion(){}
+}

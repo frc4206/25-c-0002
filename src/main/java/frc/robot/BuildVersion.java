935866f26bdb6eb899933b09c2830f4d6e84708c
Leah <26lheidt@student.nchstx.org>
2025-01-31 17:31:02 -0600

    updated wpilib

--- src/main/java/frc/robot/BuildVersion.java
+++ src/main/java/frc/robot/BuildVersion.java
@@ -7,12 +7,12 @@ public final class BuildVersion {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "25-c-0000";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1;
-    public static final String GIT_SHA = "5258f2d8c180d21ec09564aeb7d65d4d96e8b3fb";
-    public static final String GIT_DATE = "2025-01-08 20:06:17 CST";
+    public static final int GIT_REVISION = 2;
+    public static final String GIT_SHA = "44b37d6a6d557ebbbeec9f226cdcfc5485dd532e";
+    public static final String GIT_DATE = "2025-01-08 21:00:47 CST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-01-08 21:00:01 CST";
-    public static final long BUILD_UNIX_TIME = 1736391601662L;
+    public static final String BUILD_DATE = "2025-01-31 17:19:56 CST";
+    public static final long BUILD_UNIX_TIME = 1738365596098L;
     public static final int DIRTY = 1;
 
     private BuildVersion(){}

d7660b25854ba425ab9ec42360b981cc94f50194
Leah <26lheidt@student.nchstx.org>
2025-02-15 13:24:08 -0600

    untested added swerve

--- src/main/java/frc/robot/BuildVersion.java
+++ src/main/java/frc/robot/BuildVersion.java
@@ -7,13 +7,13 @@ public final class BuildVersion {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "25-c-0000";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 27;
-    public static final String GIT_SHA = "d88273168cdc11a13c4f79c073b58a799bd363f3";
-    public static final String GIT_DATE = "2025-02-14 18:57:19 CST";
-    public static final String GIT_BRANCH = "TalonFX-Rework";
-    public static final String BUILD_DATE = "2025-02-15 12:48:36 CST";
-    public static final long BUILD_UNIX_TIME = 1739645316196L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 31;
+    public static final String GIT_SHA = "a2ed54fd8cc8901c0b636ae69098d7a33e8eefb7";
+    public static final String GIT_DATE = "2025-02-15 13:13:39 CST";
+    public static final String GIT_BRANCH = "develop";
+    public static final String BUILD_DATE = "2025-02-15 13:15:34 CST";
+    public static final long BUILD_UNIX_TIME = 1739646934618L;
+    public static final int DIRTY = 0;
 
     private BuildVersion(){}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,52 +8,106 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.Percent_Com.ClawPercent_Com;
 import frc.robot.common.GameStateCurrentLimiter;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.Arm_Sub;
 import frc.robot.subsystems.Claw_Sub;
 import frc.robot.subsystems.Climber_Sub;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator_Sub;
 import frc.robot.subsystems.Intake_Sub;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import static edu.wpi.first.units.Units.*;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Climber_Sub.Config m_climbercfg = new Climber_Sub.Config("Climber.toml");
 
-
-  private final GameStateCurrentLimiter m_GameStateCurrentLimiter = new GameStateCurrentLimiter(); 
+  private final GameStateCurrentLimiter m_GameStateCurrentLimiter = new GameStateCurrentLimiter();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /* SWERVE */
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max
+                                                                                    // angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
 
-    // m_GameStateCurrentLimiter.AddSubsystem(m_climbercfg.name, m_Climber_Sub.m_climberList);
-    // m_GameStateCurrentLimiter.PopulateSubsystemLimits(m_climbercfg.name, m_Climber_Sub.currentLimitList);
+    // m_GameStateCurrentLimiter.AddSubsystem(m_climbercfg.name,
+    // m_Climber_Sub.m_climberList);
+    // m_GameStateCurrentLimiter.PopulateSubsystemLimits(m_climbercfg.name,
+    // m_Climber_Sub.currentLimitList);
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
+    drivetrain.setDefaultCommand(
+      // Drivetrain will execute this command periodically
+      drivetrain.applyRequest(() ->
+          drive.withVelocityX(-m_driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+              .withVelocityY(-m_driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+              .withRotationalRate(-m_driverController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+      )
+  );
+
+  m_driverController.back().and(m_driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+  m_driverController.back().and(m_driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+  m_driverController.start().and(m_driverController.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+  m_driverController.start().and(m_driverController.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+  m_driverController.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+  drivetrain.registerTelemetry(logger::telemeterize);
+
   }
 
   /**

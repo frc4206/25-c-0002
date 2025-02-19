92270bdf66822f2d05f5e4ae70545bc0bd28c76f
JPSeidlitz <seidfam3@gmail.com>
2025-02-19 10:55:26 -0600

    tested simulator

--- src/main/java/frc/robot/BuildVersion.java
+++ src/main/java/frc/robot/BuildVersion.java
@@ -7,13 +7,13 @@ public final class BuildVersion {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "25-c-0000";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 36;
-    public static final String GIT_SHA = "27a7336e892bc0cccd86ea54c215c14fe3f16a86";
-    public static final String GIT_DATE = "2025-02-19 09:19:43 CST";
+    public static final int GIT_REVISION = 37;
+    public static final String GIT_SHA = "77625005bb98b14a0af2f3056418ee4cfdfabc08";
+    public static final String GIT_DATE = "2025-02-19 09:22:47 CST";
     public static final String GIT_BRANCH = "Inital-Testing";
-    public static final String BUILD_DATE = "2025-02-19 09:22:07 CST";
-    public static final long BUILD_UNIX_TIME = 1739978527292L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-02-19 09:22:54 CST";
+    public static final long BUILD_UNIX_TIME = 1739978574796L;
+    public static final int DIRTY = 0;
 
     private BuildVersion(){}
 }
